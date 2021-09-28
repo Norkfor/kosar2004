edf39f7c504bfab033455eb764413faf436a3b29
Norkfor <norkfor@gmail.com>
2021-09-28 08:33:27 +0200

    MasodikFeladat megoldása, lista létrehozása, elemek beolvasása a fájlból.

--- kosar2004/Program.cs
+++ kosar2004/Program.cs
@@ -3,14 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace kosar2004
 {
   class Program
   {
+    static List<kosarak> kosar = new List<kosarak>();
     static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+      Console.ReadKey();
+
+    }
+
+    private static void HetedikFeladat()
     {
 
     }
+
+    private static void HatodikFeladat()
+    {
+
+    }
+
+    private static void OtodikFeladat()
+    {
+
+    }
+
+    private static void NegyedikFeladat()
+    {
+
+    }
+
+    private static void HarmadikFeladat()
+    {
+
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader be = new StreamReader("eredmenyek.csv");
+      be.ReadLine();
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        kosar.Add(new kosarak(a[0], (a[1]), int.Parse(a[2]), int.Parse(a[3]), a[4], DateTime.Parse(a[5])));
+      }
+      be.Close();
+    }
+
   }
 }
---
+++ kosar2004/kosarak.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+  class kosarak
+  {
+    private string hazai;
+
+    public string Hazai
+    {
+      get { return hazai; }
+      set { hazai = value; }
+    }
+
+    private string idegen;
+
+    public string Idegen
+    {
+      get { return idegen; }
+      set { idegen = value; }
+    }
+
+    private int hazai_pont;
+
+    public int Hazai_pont
+    {
+      get { return hazai_pont; }
+      set { hazai_pont = value; }
+    }
+
+    private int idegen_ponnt;
+
+    public int Idegen_pont
+    {
+      get { return idegen_ponnt; }
+      set { idegen_ponnt = value; }
+    }
+
+    private string helyszin;
+
+    public string Helyszin
+    {
+      get { return helyszin; }
+      set { helyszin = value; }
+    }
+
+    private DateTime idopont;
+
+
+
+    public DateTime Idopont
+    {
+      get { return idopont; }
+      set { idopont = value; }
+    }
+    public kosarak(string hazai, string idegen, int hazai_pont, int idegen_ponnt, string helyszin, DateTime idopont)
+    {
+      this.hazai = hazai;
+      this.idegen = idegen;
+      this.hazai_pont = hazai_pont;
+      this.idegen_ponnt = idegen_ponnt;
+      this.helyszin = helyszin;
+      this.idopont = idopont;
+    }
+
+  }
+}
